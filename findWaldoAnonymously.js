5d5b5170e25090aea5a18f38b63bd6ef8773cbe1
sarah-zhan <yizh.contact@gmail.com>
2023-02-09 19:13:33 -0800

    WaldoAnonymous

---
+++ findWaldoAnonymously.js
@@ -0,0 +1,30 @@
+
+// const findWaldo = function (names, found) {
+
+//   for (i = 0; i < names.length; i++) {
+//     let name = names[i];
+//     if (name === "Waldo") {
+//       found();   // execute callback
+//     }
+//   }
+// }
+
+const findWaldo = function(names, callback) {
+  names.forEach((e, i) => {
+    if (e === "Waldo") {
+      callback(i);
+    }
+  });
+};
+
+// const actionWhenFound = function(i) {
+//   console.log(`Found Waldo at index ${i}!`);
+// };
+
+
+
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function (i) {
+  console.log(`Found Waldo at index ${i}!`);
+});
